3431d8ad23b2f104ebbc0c347c62daff5634cf92
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-10-09 15:02:23 -0400

    Main

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// global variables
+var trainer = false;
+var count = 0;
+
+/* Main */
+function main(){
+    let again = false;
+    trainer = confirm("Trainer mode?");
+    playNim();
+    again = confirm("Again?");
+    if (again == true){
+        main();
+    }
+    alert("Tahnk you for playing!");
+}
+
+/* playNim */
+function playNim(){
+    alert("You played Nim");
+}
 
