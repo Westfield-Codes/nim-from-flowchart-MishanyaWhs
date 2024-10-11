d166310a454719e814aaea75630a2f002c695eea
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-10-11 11:23:47 -0400

    Main

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-/* Nim Trainer by Mykhailo M
+/* Nim Trainer by Mykhailo M.
+ * Chart:https://lucid.app/lucidchart/944cff40-8a5a-4e07-b5dd-a06243be5140/edit?invitationId=inv_9088d170-bb1d-4d8b-b0b3-91a175e9cb05&page=lVogpd-J6AOb#
+*/
+
+/* Global Variables */
+var trainer = false;
+var count = 0;
 
 /** 
- * Main  * 
+ * Main * 
  * @param parameter1 Description of the first parameter. 
  * @return Description of the return value. 
  */
-
-// global variables
-var trainer = false;
-var count = 0;
 
 /* Main */
 function main(){
@@ -39,11 +41,18 @@ function playNim(){
  * @return Description of the return value. 
  */
 
+function userTurn(){
+    count += 3;
+    alert("You counted 3. count is now " + count);
+}
+
 /** 
  * cpuTurn gets a computer's turn without losing on purpose. Different turn   * 
  * @param parameter1 Description of the first parameter. 
  * @return Description of the return value. 
  */
 
-
-
+function cpuTurn(){
+    count += 1;
+    alert("I counted 1. count is now " + count);
+}
